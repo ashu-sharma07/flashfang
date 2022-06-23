25655acc1fc3e9c53d742f810eb53d90828a2574
Ashu Sharma <ashusharma0771@gmail.com>
2022-06-23 22:34:14 +0530

    public and views ejs setup complete

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const bodyParser = require("body-parser");
 
 // Inital App Set up 
 const app = express();
+
+// Ejs Setup 
+app.set('view engine', 'ejs');
+
+// Express static files Setup
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 //  Handle get request to root route
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.sendFile(__dirname+"/public/index.html");
 });
 
 // Starting app on 3000
